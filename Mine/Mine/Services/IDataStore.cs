d20252a1427ac144da823279743406212f521525
olearyfiona <94136189+olearyfiona@users.noreply.github.com>
2022-01-25 14:51:27 -0800

    Renamed GetItemAsync to ReadAsync in MockDataStore.cs, IDataStore.cs

--- Mine/Mine/Services/IDataStore.cs
+++ Mine/Mine/Services/IDataStore.cs
@@ -10,6 +10,6 @@ namespace Mine.Services
         Task<bool> UpdateItemAsync(T item);
         Task<bool> DeleteItemAsync(string id);
         Task<T> GetItemAsync(string id);
-        Task<IEnumerable<T>> GetItemsAsync(bool forceRefresh = false);
+        Task<IEnumerable<T>> ReadAsync(bool forceRefresh = false);
     }
 }
