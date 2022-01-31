171690d7620e8d1fa1d9a8b416d4ad31fb19fe00
olearyfiona <94136189+olearyfiona@users.noreply.github.com>
2022-01-31 15:53:18 -0800

    Added Constants class to the Shared Project, Constants.cs

---
+++ Constants.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Constants
+{
+	public const string DatabaseFilename = "TodoSQLite.db3";
+
+	public const SQLite.SQLiteOpenFlags Flags =
+		//open the database in read/write mode
+		SQLite.SQLiteOpenFlags.ReadWrite |
+		//create database if it doesnt exist
+		SQLite.SQLiteOpenFlags.Create |
+		//enable multi-threaded database access
+		SQLite.SQLiteOpenFlags.SharedCache;
+
+	public static string DatabasePath
+    {
+        get
+        {
+			var basePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+			return basePath.Combine(basePath, DatabaseFilename);
+        }
+    }
+
+
+}
