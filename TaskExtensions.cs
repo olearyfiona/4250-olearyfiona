4a51f5e3055bb762eea769e9af03f4e425deddb7
olearyfiona <94136189+olearyfiona@users.noreply.github.com>
2022-01-31 16:24:32 -0800

    Added TaskExtension to shared project, TaskExtensions.cs

---
+++ TaskExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Mine
+{
+    public static class TaskExtensions
+    {
+        //NOTE Async void is intentional here. This provides a way to call
+        //an async methods from the constructor while communicating intent 
+        //to fire and forget, and allow handling exceptions
+        public static async void SafeFireAndForget(this Task task,
+                bool returnToCallingContext,
+                Action<Exception> onException = null)
+        {
+            try
+            {
+                await task.ConfigureAwait(returnToCallingContext);
+            }
+
+            //if the provided action is not null, catch and pass the thrown exception
+            catch (Exception ex) when (onException != null)
+            {
+                onException(ex);
+            }
+        }
+    }
+    
+}
+
+
+
