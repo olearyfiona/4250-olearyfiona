41750d2338bac78ca8ea522bf2ce7aa664aa4b32
olearyfiona <94136189+olearyfiona@users.noreply.github.com>
2022-02-05 10:11:07 -0800

    Updated UT to use new ItemModel in ItemModelTests.cs

--- UnitTests/Models/ItemModelTests.cs
+++ UnitTests/Models/ItemModelTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using NUnit.Framework;
+using Mine.Models;
 
 namespace UnitTests.Models
 {
@@ -14,9 +15,12 @@ namespace UnitTests.Models
             //Arrange
 
             //Act
+            var result = new ItemModel();
+
+            //Reset
 
             //Assert
-            Assert.Pass();
+            Assert.IsNotNull(result);
         }
     }
 }
